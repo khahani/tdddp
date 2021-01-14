a5db6ccc2365cf121689e72b88ae47a0f0bcc849
Mohammadreza Khahani <mr.khahani@gmail.com>
2021-01-14 13:44:25 +0330

    Simple client usage of composite without builder.

--- src/composite/Client.java
+++ src/composite/Client.java
@@ -2,6 +2,17 @@ package composite;
 
 public class Client {
     public static void main(String[] args) {
+        Branch root = new Branch("root");
+        root.add(new Leaf("green"));
+        root.add(new Leaf("green"));
 
+        Branch b1 = new Branch("level 1");
+        b1.add(new Leaf("yellow"));
+        b1.add(new Leaf("yellow"));
+        b1.add(new Leaf("yellow"));
+
+        root.add(b1);
+
+        System.out.println(root.describe(1));
     }
 }
