019c7292191e08124b8881f41aa2c2edf2ad4f2a
Mohammadreza Khahani <mr.khahani@gmail.com>
2022-01-18 08:54:52 +0330

    add mergesort

--- src/algorithm/sort/mergesort/MergeSort.java
+++ src/algorithm/sort/mergesort/MergeSort.java
@@ -7,9 +7,14 @@ import java.util.Arrays;
 public class MergeSort extends SortAlg {
 
     public static void main(String[] args) {
+        MergeSort ms = new MergeSort();
+
+        final Integer[] b = {2, 1};
+        ms.sort(b);
+        System.out.println(Arrays.toString(b));
+
         final Integer[] a = {1, 3, 5, 7, 2, 4, 6, 8};
         final Integer[] aux = new Integer[a.length];
-        MergeSort ms = new MergeSort();
         ms.merge(a, aux, 0, 3, a.length - 1);
         System.out.println(Arrays.toString(a));
 
@@ -18,31 +23,15 @@ public class MergeSort extends SortAlg {
         System.out.println(Arrays.toString(array));
     }
 
-    @Override
-    public void sort(Comparable[] a) {
-        final Comparable[] aux = new Comparable[a.length];
-        sort(a, aux, 0, a.length - 1);
-    }
-
-    private void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
-        if (hi <= lo)
-            return;
-        int mid = lo + (hi - lo) / 2;
-        sort(a, aux, lo, mid);
-        sort(a, aux, mid + 1, hi);
-        merge(a, aux, lo, mid, hi);
-    }
-
     private void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
         assert isSorted(a, lo, mid);
-        assert isSorted(a, mid, hi);
+        assert isSorted(a, mid + 1, hi);
 
         for (int k = lo; k <= hi; k++)
             aux[k] = a[k];
 
         int i = lo;
         int j = mid + 1;
-
         for (int k = lo; k <= hi; k++) {
             if (i > mid)
                 a[k] = aux[j++];
@@ -55,5 +44,20 @@ public class MergeSort extends SortAlg {
         }
 
         assert isSorted(a, lo, hi);
+    }
+
+
+    @Override
+    public void sort(Comparable[] a) {
+        Comparable[] aux = new Comparable[a.length];
+        sort(a, aux, 0, a.length - 1);
+    }
+
+    private void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+        if (lo >= hi) return;
+        int mid = lo + (hi - lo) / 2;
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid + 1, hi);
+        merge(a, aux, lo, mid, hi);
     }
 }
